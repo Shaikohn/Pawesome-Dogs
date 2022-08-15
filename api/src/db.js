96efd23bc42d59e55924f323967ebe7a97a85ae1
unknown <shaicokohn@gmail.com>
2022-08-15 16:24:26 -0300

    Update

--- api/src/db.js
+++ api/src/db.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+const {pg} = require("pg")
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speedzz
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectModule: pg
 });
 const basename = path.basename(__filename);
 
